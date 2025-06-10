8ef00463c4e3f8978c4bd44b293bb3c561d5df11
heidrich76 <jens.heidrich@gmail.com>
2025-06-10 21:14:09 +0200

    Added rootpath argument

--- app/main.py
+++ app/main.py
@@ -16,31 +16,37 @@ def index_page():
 
 app.add_static_files("/static", "static")
 
-parser = argparse.ArgumentParser(description="Start the MEGAcmd NiceGUI app.")
-parser.add_argument("--host", default="0.0.0.0", help="Host address to bind to")
-parser.add_argument("--port", type=int, default=8080, help="Port number to bind to")
-parser.add_argument(
-    "--reload", type=bool, default=False, help="Enable autoreload (development mode)"
-)
-parser.add_argument(
-    "--show", type=bool, default=False, help="Show browser window on start"
-)
-parser.add_argument(
-    "--log-level",
-    default="error",
-    choices=["critical", "error", "warning", "info", "debug", "trace"],
-    help="Uvicorn logging level",
-)
-
-args = parser.parse_args()
-
-try:
-    ui.run(
-        host=args.host,
-        port=args.port,
-        reload=args.reload,
-        show=args.show,
-        uvicorn_logging_level=args.log_level,
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Start the MEGAcmd NiceGUI app.")
+    parser.add_argument("--host", default="0.0.0.0", help="Host address to bind to")
+    parser.add_argument("--port", type=int, default=8080, help="Port number to bind to")
+    parser.add_argument(
+        "--reload",
+        type=bool,
+        default=False,
+        help="Enable autoreload (development mode)",
     )
-except KeyboardInterrupt:
-    print("NiceGUI server stopped via keyboard interrupt")
+    parser.add_argument(
+        "--show", type=bool, default=False, help="Show browser window on start"
+    )
+    parser.add_argument(
+        "--loglevel",
+        default="error",
+        choices=["critical", "error", "warning", "info", "debug", "trace"],
+        help="Uvicorn logging level",
+    )
+    parser.add_argument("--rootpath", default="", help="Root path to use")
+
+    args = parser.parse_args()
+
+    try:
+        ui.run(
+            host=args.host,
+            port=args.port,
+            reload=args.reload,
+            show=args.show,
+            uvicorn_logging_level=args.loglevel,
+            root_path=args.rootpath,
+        )
+    except KeyboardInterrupt:
+        print("NiceGUI server stopped via keyboard interrupt")
